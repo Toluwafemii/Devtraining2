6be78b2a3b57fb6e632c0bd6e10222aa7650fb34
Femi <davidokaseun@gmail.com>
2017-07-09 17:50:38 +0100

    add image img/New york.jpg

---
+++ forms.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <header>
+        <meta name="keywords" content="width=device-width initial scale=1">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/font-awesome.css">
+        <link rel="stylesheet" href="css/Custom.css">
+        <link rel="stylesheet" type="text/css" href="style.css" media="all" />
+        <link rel="stylesheet" type="text/css" href="style.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="demo.css" media="all" />
+    </header>
+    <body id="wrapper">
+        
+        <div class="container">
+        </div>
+             
+        <!-- Script -->
+         <script type="text/javascript" src="js/jquery-3.2.1.min.js.js"></script>
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.js.js"></script>
+        <script type="text/javascript" src="js/jquery.flexslider-min.js"></script>
+        <script type="text/javascript" src="js/jquery.flexslider-min.js"></script>
+        <!-- Script End -->
+    </body>
+    <footer>
+    </footer>
+</html>
